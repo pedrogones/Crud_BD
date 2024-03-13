b830debff48fc193166f563b754ea64f239edb4a
pedro gones <pedrotargin@gmail.com>
2024-03-12 23:39:14 -0300

    Update implements

--- src/app/consultas/update/update.component.ts
+++ src/app/consultas/update/update.component.ts
@@ -2,84 +2,103 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { SharedService } from '../../../shared/shared.service';
 import { FormsModule } from '@angular/forms';
 import { Consultas } from '../../models/Consultas';
+import { MatFormField, MatFormFieldModule, MatHint } from '@angular/material/form-field';
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
 import { DatePickerComponent } from '../../../services/date-picker/date-picker.component';
 import { CommonModule } from '@angular/common';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../../services/auth.service';
+import { delay } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-  ],
+  imports: [CommonModule,FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.scss'], // Corrigido para `styleUrls`
+  styleUrl: './update.component.scss'
 })
 export class UpdateComponent implements OnInit {
   @ViewChild(DatePickerComponent)
   datePickerComponent!: DatePickerComponent<any>;
   exampleHeader = DatePickerComponent;
-
-  constructor(private sharedService: SharedService){
-
-  }
-  hora = '08:30';
-  minDate = new Date();
-  consulta: Consultas = {
-    idConsulta: 1,
-    nomePaciente: 'Pedro Gomes',
-    nomeMedico: 'João Marcos',
-    dataConsulta: '20/03, 08:30',
-    motivoConsulta: 'Revisão Geral',
+  consultaPage: Consultas = {
+    idConsulta: 0,
+    nomePaciente: '',
+    nomeMedico: '',
+    dataConsulta: '',
+    motivoConsulta: ''
   };
+  minDate= Date()
+  hora!:string
 
-  ngOnInit() {}
+  constructor(
+    private sharedService: SharedService,
+    private http: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
-  validate_inputs(consulta: Consultas): boolean {
-    if (
-      consulta.nomePaciente == '' ||
-      consulta.nomeMedico == '' ||
-      consulta.dataConsulta == '' ||
-      consulta.motivoConsulta == ''
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-<<<<<<< HEAD
-  backHome(){
-    this.sharedService.home()
-  }
-  save(){
-    //implements
-=======
-
-  save(): void {
-    if (this.consulta.dataConsulta instanceof Date) {
-      const year = this.consulta.dataConsulta.getUTCFullYear();
-      const month = this.consulta.dataConsulta.getUTCMonth() + 1;
-      const day = this.consulta.dataConsulta.getUTCDate();
-      const dataConsultaF = day + '-' + month + '-' + year;
-      this.consulta.dataConsulta = dataConsultaF + ', ' + this.hora;
-    }
-    if (this.validate_inputs(this.consulta)) {
-      console.log(this.consulta);
-    } else {
-      console.log('Preencha todos os campos');
-    }
->>>>>>> 49dce922ad689bb2196d2c67f478315e6fc3f2a1
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const consultaId = +params['id']; // Use o operador + para converter o ID para número, se necessário
+      this.http.loadByID(consultaId).subscribe(
+      
+        consulta => {
+          // Aqui você recebe o valor real da consulta quando o Observable for concluído
+          this.consultaPage = consulta;
+          console.log(this.consultaPage);
+        },
+        error => {
+          console.error('Ocorreu um erro ao carregar a consulta por ID:', error);
+        }
+      );
+    });
   }
 
   backHome() {
     this.sharedService.home();
   }
+
+  save() {
+    console.log(this.consultaPage.dataConsulta)
+    if (this.consultaPage.dataConsulta instanceof Date) {
+      // Obtendo os componentes da data
+      const horaFormate = this.hora
+      const year = this.consultaPage.dataConsulta.getUTCFullYear();
+      const month = (this.consultaPage.dataConsulta.getUTCMonth() + 1).toString().padStart(2, '0'); // Mês começa em 0
+      const day = this.consultaPage.dataConsulta.getUTCDate().toString().padStart(2, '0');
+      const seconds = this.consultaPage.dataConsulta.getUTCSeconds().toString().padStart(2, '0');
+      const dataConsultaFormatada = `${year}-${month}-${day}T${horaFormate}:${seconds}`;
+      // Atualizar a propriedade dataConsulta
+      console.log(this.hora)
+      this.consultaPage.dataConsulta = dataConsultaFormatada;
+      console.log(dataConsultaFormatada);
+    }
+  
+    console.log(this.consultaPage);
+  
+    if (this.consultaPage.idConsulta !== null && this.consultaPage.idConsulta !== undefined) {
+      console.log(this.consultaPage)
+      this.http.update(this.consultaPage).pipe(
+        delay(2000)
+        ).subscribe(
+          () => {
+          this.sharedService.home();
+        },
+        error => {
+          console.error('Ocorreu um erro ao atualizar a consulta:', error);
+        }
+      );
+    } else {
+      console.error('ID da consulta é nulo ou indefinido.');
+    }
+  }
+  
+onCancel(){
+ this.sharedService.consultas()
+}
+
+
 }
