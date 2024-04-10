cff1cc728696121258b1b2a51f83284422fbf706
pedro gones <pedrotargin@gmail.com>
2024-04-10 12:53:15 -0300

    add login logical validate inputs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,21 @@
 import { CreateComponent } from './consultas/create/create.component';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'
 import { NgModule } from '@angular/core';
 import { UpdateComponent } from './consultas/update/update.component';
 import { ConsultasComponent } from './consultas/consultas.component';
 import { LoginComponent } from './auth/login/login.component';
-
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path:'login', component:LoginComponent},
-  {path: 'consultas', component: ConsultasComponent},
-  {path: 'consultas/create', component: CreateComponent},
-  { path:'consultas/update/:id', component: UpdateComponent }
-
+  { path: 'login', component: LoginComponent
+  },
+  { path: '', component: HomeComponent },
+  { path: 'consultas', component: ConsultasComponent },
+  { path: 'consultas/create', component: CreateComponent },
+  { path: 'consultas/update/:id', component: UpdateComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,40 +1,90 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SharedService } from '../../../shared/shared.service';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { AuthService } from '../../../services/auth.service';
+import { merge, takeUntil } from 'rxjs';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatFormField],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
-  constructor(private sharedService: SharedService){
-
-  }
-
-  cpf:any
   authUser = 'medico';
-  login(){
-//    if(key=='medico'){
-//    }
-    this.sharedService.consultas()
+  errorMessage = '';
+  emailErrorMessage='';
+  nomeErrorMessage='';
+  cpfErrorMessage='';
+  // declarações dos inputs
+  email = new FormControl('', [Validators.required, Validators.email])
+  senha = new FormControl('', [Validators.required, Validators.minLength(6)])
+  nome = new FormControl('', [Validators.required])
+  cpf = new FormControl('', [Validators.required, Validators.minLength(11)])
+  loginFormMedico: { email: string|null; password: string|null } = {email: '', password: ''};
+  loginFormPaciente: { nome: string|null; cpf: string|null } = { nome: '', cpf: '' };
+  //
+  constructor(private sharedService: SharedService, private http: AuthService){
+    merge(this.email.statusChanges, this.email.valueChanges).subscribe(()=>this.updateErrorMessage(this.email));
+    merge(this.senha.statusChanges, this.senha.valueChanges).subscribe(()=>this.updateErrorMessage(this.senha));
+    merge(this.nome.statusChanges, this.nome.valueChanges).subscribe(()=>this.updateErrorMessage(this.nome));
+    merge(this.cpf.statusChanges, this.cpf.valueChanges).subscribe(()=>this.updateErrorMessage(this.cpf));
+
   }
-  submitForm() {
-    // Implemente a lógica de autenticação para médicos aqui
+  //validações de inputs 
+  updateErrorMessage(field: FormControl){
+    if(field.hasError('required')){
+      this.errorMessage = 'Esse campo é obrigatório!';
+      this.emailErrorMessage = 'Esse campo é obrigatório!';
+      this.nomeErrorMessage = 'Esse campo é obrigatório!';
+      this.cpfErrorMessage = 'Esse campo é obrigatório!';
+      
+    } else if(field.hasError('email')){
+      this.emailErrorMessage = 'Coloque um email válido!';
+    } else if(field.hasError('minlength')){
+      this.errorMessage = 'Deve conter no mínimo 6 caracteres';
+      this.cpfErrorMessage = 'Cpf deve conter no mínimo 11 caracteres';
+    } else{
+      this.errorMessage = '';
+    }
+}
+//chamadas do metodo http
+  loginMedico(authUser: string, email:FormControl, senha: FormControl) {
+    if (authUser === 'medico') {
+      this.loginFormMedico.email = this.email.value 
+      this.loginFormMedico.password = this.senha.value
+      if(email.invalid||senha.invalid){
+        this.sharedService.openDialog("Preencha os campos!")
+      }else{ 
+        console.log(this.loginFormMedico)
+        //this.http.loginMedico(email, senha)
+      }
+    }else{
+      this.sharedService.openDialog("Ocorreu um erro")
+    }
   }
-
-  // Função para submeter o formulário de paciente
-  submitPacienteForm() {
-    // Implemente a lógica de autenticação para pacientes aqui
+loginPaciente(authUser: string, nome: FormControl, cpf: FormControl){
+  if (authUser === 'paciente') {
+    this.loginFormPaciente.nome = this.nome.value 
+      this.loginFormPaciente.cpf = this.cpf.value
+      if(nome.invalid||cpf.invalid){
+        this.sharedService.openDialog("Preencha os campos!")
+      }else{ 
+        console.log(this.loginFormMedico)
+        //this.http.loginMedico(email, senha)
+      }
+  } else {
+    this.sharedService.openDialog("Ocorreu um erro!");
   }
-
-
-
+} 
   changeDisplay(typeUser: string) {
      this.authUser = typeUser
   }
 
 }
+
+
