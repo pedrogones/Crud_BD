45887f1361e627e330eb6e5bddeedc8e2da21342
pedro gones <pedrotargin@gmail.com>
2024-04-24 14:58:56 -0300

    home adjusted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,18 @@ import { SharedModule } from '../shared/shared.module';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthUser } from '../models/AuthUser';
 import { Medico } from '../models/Medico';
-import { delay } from 'rxjs';
+import { Observable, delay } from 'rxjs';
 import { ModalInfoConsultaComponent } from './modal-info-consulta/modal-info-consulta.component';
 import { FormsModule } from '@angular/forms';
 import { Paciente } from '../models/Paciente';
-import { ActivatedRoute } from '@angular/router';
 import { PermissionsService } from '../controller/permissions.service';
 import { ModalExportReportComponent } from './modal-export-report/modal-export-report.component';
-import { ModalHistoryComponent } from './modal-history/modal-history.component';
+import { ConsultasService } from '../../services/consultasServices/consultas.service';
+import { ConsultaRequest } from '../models/ConsultaRequest';
+import { MedicosService } from '../../services/medicoServices/medicos.service';
+import { error, log } from 'console';
+import { MatTableDataSource } from '@angular/material/table';
+import { PacienteService } from '../../services/pacientesServices/paciente.service';
 
 @Component({
   selector: 'app-home',
@@ -25,134 +29,102 @@ import { ModalHistoryComponent } from './modal-history/modal-history.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-
-  authUser: AuthUser={
-    id: 0,
-    nome: '',
-    role: 5,
-    chavePrimaria:'111.111.111-11'
-  }
-  pacientes:Paciente[]=[{
-    idPaciente: 1,
-    nome: 'Pedro Krl',
-    cpf: '123.123.123-21',
-    role: 0
-  },{
-    idPaciente: 2,
-    nome: 'HAbudabu aspin',
-    cpf: '234.522.123-21',
-    role: 0
-  },
-  {
-    idPaciente: 8,
-    nome: 'Prasputin',
-    cpf: '635.045.123-21',
-    role: 0
-  }]
-  medicos:Medico[]=[{
-    idMedico:1,
-  nome: 'Joao Gomes',
-  email: 'string',
-  senha: 'string',
-  especialidade: 'string',
-  role: 1,
-  crm: '12312/PB'
-  },
-  {
-    idMedico:2,
-    nome: 'Pedro Gomes',
-    email: 'string',
-    senha: 'string',
-    especialidade: 'asdasd',
-    role: 1,
-    crm: '12222/PB'
-    }
-]
+  pacientes:Paciente[]=[]
+  medicos: Medico[] = [];
+  roleUser: any;
+  pacienteEmpty=false
+  medicoEmpty=false
+  pkUser: any;
+  pacienteLogado!: Paciente;
+  dataSource = new MatTableDataSource<ConsultaRequest>();
+  displayedColumns: string[] = ['idConsulta', 'nomePaciente', 'nomeMedico', 'dataConsulta', 'motivoConsulta'];
   consultaIsEmpty = true;
-  isContentVisible = false
+  isContentVisible = false;
+  consultas: ConsultaRequest[] = [];
+
+  constructor( public dialog: MatDialog, private sharedService: SharedService, private http: AuthService, private httpConsult: ConsultasService, private medicoService: MedicosService, private pacienteService: PacienteService) { }
+
   ngOnInit(): void {
-    this.authUser.role = this.role.getRole()
-    this.authUser.chavePrimaria = this.role.getChavePrimaria()
-    console.log('role: '+this.authUser.role+'\nPrimari key:'+this.authUser.chavePrimaria)
-
-
+    if (typeof localStorage !== 'undefined') {
+      this.pkUser = localStorage.getItem('chavePrimaria');
+      this.roleUser = Number(localStorage.getItem('role'));
+    } else {
+      return
+    }
+    console.log('role: ' + this.roleUser + '\nPrimari key:' + this.pkUser);
     setTimeout(() => {
-      this.isContentVisible = true; // Define como true após o atraso de 5 segundos
+      this.isContentVisible = true;
       this.index();
     }, 1500);
 
-  }
-  constructor(private role: PermissionsService, public dialog: MatDialog, private sharedService: SharedService, private http: AuthService, private cdr: ChangeDetectorRef) { }
-
-  consultas: Consultas[] = [];
-  openModal(idConsulta: any) {
-    const dialogRef = this.dialog.open(ModalInfoConsultaComponent, {
-      data: { idConsulta: idConsulta } // Passa o idConsulta como dado para a modal
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      this.index();
-    });
-  }
-  index() {
-   this.buscarConsultasPorRole(this.authUser.role)
-  }
-  openDialog(message: string) {
-    this.sharedService.openDialog(message)
-  }
-  delete(id: number) {
-  }
-  converterData(data: any): string {
-    return this.sharedService.converterData(data)
+    if(this.roleUser!=1){
+      this.medicoService.index().subscribe((data: Medico[]) => {
+        this.medicos = data;
+      }, (error) => {
+        console.log(error);
+      });
+    }
+    if(this.roleUser!=0){
+      this.pacienteService.index().subscribe((data: Paciente[])=>{
+        this.pacientes = data;
+      },(erro)=>{
+        console.log(erro)
+      }
+    )
+    }
   }
 
-  redirectCreate() {
-    this.sharedService.create();
-  }
-  redirectUpdate(id: number) {
-    this.sharedService.update(id);
-  }
-  //permissions
-  /*
-  *
-   * nesta section vamos tratar as permissões
-   * 0 -> Paciente
-   * 1 -> Médico
-   * 2 - > Administrador
-   *
-  */
- pacienteEmpty = false;
- medicoEmpty = false
- dataFilter= new Date()
+dataFilter = ''
+openModal(idConsulta: any) {
+  const dialogRef = this.dialog.open(ModalInfoConsultaComponent, {
+    data: { idConsulta: idConsulta }
+  });
+  dialogRef.afterClosed().subscribe(result => {
+    this.index();
+  });
+}
+replaceSpaceString(nome: string): string {
+  return nome.replace(/\s+/g, '_');
+}
   buscarConsultasPorRole(role: number) {
-    console.log(this.sharedService.formatData(this.dataFilter))
-    let consultaObservable;
+    let consultaObservable: Observable<ConsultaRequest[]>;
     if (role === 0) {
-      consultaObservable = this.http.consultasPorDataPaciente(this.authUser.chavePrimaria, this.dataFilter);
-    } else if(role === 1){
-      consultaObservable = this.http.consultasPorDataMedico(this.authUser.chavePrimaria, this.dataFilter)
+      consultaObservable = this.httpConsult.listarConsultasPaciente(this.pkUser);
+    } else if (role === 1) {
+      consultaObservable = this.httpConsult.listarConsultasPorCrm(this.pkUser);
     } else if (role === 2) {
-      //Vamos usar esse: consultaObservable = this.http.consultasPorData(this.dataFilter);
-      consultaObservable = this.http.index();
+      consultaObservable = this.httpConsult.index();
     } else {
       return;
     }
     consultaObservable.subscribe(
-      (data: Consultas[]) => {
+      (data: ConsultaRequest[]) => {
         this.consultas = data;
+        this.dataSource.data = this.consultas;
         this.consultaIsEmpty = this.consultas.length === 0;
-        delay(3000); // Talvez você queira remover isso, pois pode não fazer sentido aqui
+        if (data.length === 0) {
+         if(this.roleUser==0){
+          this.pacienteEmpty = true;
+          this.isContentVisible = false;
+          this.sharedService.openDialog('Nenhuma consulta encontrada!');
+         } else if(this.roleUser==1){
+            this.medicoEmpty = true;
+            this.isContentVisible = false;
+            this.sharedService.openDialog('Nenhuma consulta encontrada!');
+           }
+        }        delay(3000); // Talvez você queira fazer algo diferente com o atraso aqui.
       },
       error => {
         let errorMessage;
         switch (role) {
           case 0:
-            this.pacienteEmpty=true
-            this.isContentVisible=false
+            this.pacienteEmpty = true;
+            this.isContentVisible = false;
             break;
           case 1:
-            this.medicoEmpty=true
-            this.isContentVisible=false
-           break;
+            this.medicoEmpty = true;
+            this.isContentVisible = false;
+            break;
           case 2:
             errorMessage = 'Ocorreu um erro ao listar as consultas, tente novamente mais tarde';
             this.openDialog(errorMessage);
@@ -160,52 +132,71 @@ export class HomeComponent implements OnInit {
           default:
             errorMessage = 'Ocorreu um erro, tente novamente mais tarde';
             break;
-          }
+        }
       }
     );
   }
-  menuData = false
-  menuPaciente = false
-  //filtrar as consultas por medico selecionado
+  menuData = false;
+  menuPaciente = false;
   menuMedico = false;
-  openSelect(){
-    this.menuMedico = true
+
+  openSelect() {
+    this.menuMedico = true;
   }
 
-  hoje = new Date()
+  hoje = new Date();
   minDate = this.hoje.toISOString().split('T')[0];
-  buscarPorMedico(crm: string) {
+
+  buscarPorMedico(nome: string) {
     this.menuMedico = false;
-    this.http.consultasPorDataMedico(crm, this.dataFilter).subscribe(
-      (data: Consultas[]) => {
-        this.consultas = data;
+    if(nome==""){ this.index(); return}
+    this.httpConsult.listarConsultasMedico(nome).subscribe(
+      (data: ConsultaRequest[]) => {
+        if (data && data.length > 0) {
+          this.consultas = data;
+          this.dataSource.data = this.consultas; // Atualiza a fonte de dados da tabela
+          this.consultaIsEmpty = false;
+        } else {
+          this.index();
+          this.sharedService.openDialog('Não há consultas para o médico selecionado.');
+        }
       },
       error => {
-        this.openDialog('Ocorreu um erro ao buscar as consultas do médico, tente novamente mais tarde');
+        this.sharedService.openDialog('Ocorreu um erro ao buscar as consultas do médico, tente novamente mais tarde');
       }
     );
   }
-
+  
   buscarConsultasPorData(data: any) {
+   console.log(data)
     this.menuData = false;
-    this.http.consultasPorDataPaciente(this.authUser.chavePrimaria, data).subscribe(
-      (data: Consultas[]) => {
-        this.consultas = data;
-      },
-      error => {
-        this.openDialog('Ocorreu um erro ao buscar as consultas por data, tente novamente mais tarde');
+    this.httpConsult.listarConsultasPorData(data).subscribe(
+      (data: ConsultaRequest[]) => {
+        if (data && data.length > 0) {
+          this.consultas = data;
+          this.dataSource.data = this.consultas;
+          this.consultaIsEmpty = false;
+        } else {
+          this.index();
+          this.sharedService.openDialog('Não há consultas para o médico selecionado.');
+        }
       }
     );
+   
   }
 
-  buscarPorPaciente(cpf: any) {
+  buscarPorPaciente(nome: any) {
     this.menuPaciente = false;
-    this.http.consultasPorPaciente(cpf).subscribe(
-      (data: Consultas[]) => {
-        this.consultas = data;
-      },
-      error => {
-        this.openDialog('Ocorreu um erro ao buscar as consultas do paciente, tente novamente mais tarde');
+    this.httpConsult.listarConsultasPaciente(nome).subscribe(
+      (data: ConsultaRequest[]) => {
+        if (data && data.length > 0) {
+          this.consultas = data;
+          this.dataSource.data = this.consultas; // Atualiza a fonte de dados da tabela
+          this.consultaIsEmpty = false;
+        } else {
+          this.index();
+          this.sharedService.openDialog('Não há consultas.');
+        }
       }
     );
   }
@@ -215,11 +206,23 @@ export class HomeComponent implements OnInit {
 
     });
   }
-  openModalHistorico(){
-    const dialogRef = this.dialog.open(ModalHistoryComponent);
-    dialogRef.afterClosed().subscribe(result => {
-
-    });
-  }
+  index() {
+    this.buscarConsultasPorRole(this.roleUser)
+   }
+   openDialog(message: string) {
+     this.sharedService.openDialog(message)
+   }
+   delete(id: number) {
+   }
+   converterData(data: any): string {
+     return this.sharedService.converterData(data)
+   }
+ 
+   redirectCreate() {
+     this.sharedService.createRole(this.roleUser, this.pkUser);
+   }
+   redirectUpdate(id: number) {
+     this.sharedService.updateRole(id, this.roleUser, this.pkUser);
+   }
 
 }
